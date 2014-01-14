7bdd27ce544129765c686b9791271e8decb5f5da
SeongJae Park <sj38.park@gmail.com>
2014-01-14 15:22:37 +0900

    Apply go fmt
    Let's go with go fmt.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- anal.go
+++ anal.go
@@ -1,86 +1,86 @@
 package main
 
 import (
-        "bytes"
-        "encoding/json"
-        "fmt"
-        "io/ioutil"
-        "log"
-        "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 const (
-    GerritURL = "https://android-review.googlesource.com"
-    FirstSortKey = "002a4ecd00011ff0"
+	GerritURL    = "https://android-review.googlesource.com"
+	FirstSortKey = "002a4ecd00011ff0"
 )
 
 func pindent(depth int, str string) {
-    for i := 0; i < depth; i++ {
-        fmt.Print("    ")
-    }
-    fmt.Print(str)
+	for i := 0; i < depth; i++ {
+		fmt.Print("    ")
+	}
+	fmt.Print(str)
 }
 
 func printRawMarshalled(infos interface{}, depth int) {
-    switch vinfos := infos.(type) {
-    case map[string]interface{}:
-        pindent(depth, "{\n")
-        for k, v := range vinfos {
-            pindent(depth + 1, "")
-            switch vv := v.(type) {
-            case string:
-                fmt.Printf("%v: (string) - %q\n", k, vv)
-            case int:
-                fmt.Printf("%v: (int) - %q\n", k, vv)
-            default:
-                fmt.Printf("%v: (not string, neither int)\n", k)
-                printRawMarshalled(v, depth + 1)
-            }
-        }
-        pindent(depth, "}\n")
-    case []interface{}:
-        pindent(depth, "[\n")
-        for k, v := range vinfos {
-            pindent(depth + 1, "")
-            switch vv := v.(type) {
-            case string:
-                fmt.Printf("%vth: (string) - %q\n", k, vv)
-            case int:
-                fmt.Printf("%vth: (int) - %q\n", k, vv)
-            default:
-                fmt.Printf("%vth: (not string, neither int)\n", k)
-                printRawMarshalled(v, depth + 1)
-            }
-        }
-        pindent(depth, "]\n")
-    }
+	switch vinfos := infos.(type) {
+	case map[string]interface{}:
+		pindent(depth, "{\n")
+		for k, v := range vinfos {
+			pindent(depth+1, "")
+			switch vv := v.(type) {
+			case string:
+				fmt.Printf("%v: (string) - %q\n", k, vv)
+			case int:
+				fmt.Printf("%v: (int) - %q\n", k, vv)
+			default:
+				fmt.Printf("%v: (not string, neither int)\n", k)
+				printRawMarshalled(v, depth+1)
+			}
+		}
+		pindent(depth, "}\n")
+	case []interface{}:
+		pindent(depth, "[\n")
+		for k, v := range vinfos {
+			pindent(depth+1, "")
+			switch vv := v.(type) {
+			case string:
+				fmt.Printf("%vth: (string) - %q\n", k, vv)
+			case int:
+				fmt.Printf("%vth: (int) - %q\n", k, vv)
+			default:
+				fmt.Printf("%vth: (not string, neither int)\n", k)
+				printRawMarshalled(v, depth+1)
+			}
+		}
+		pindent(depth, "]\n")
+	}
 }
 
 func fetchChanges(status string, next_sort_key string) interface{} {
-    dest_url := fmt.Sprintf("%s/changes/?q=status:%s&n=25&O=1&N=%s",
-            GerritURL, status, next_sort_key)
-    resp, err := http.Get(dest_url)
-    if err != nil {
-        log.Fatal("fetch from net fail", err)
-    }
+	dest_url := fmt.Sprintf("%s/changes/?q=status:%s&n=25&O=1&N=%s",
+		GerritURL, status, next_sort_key)
+	resp, err := http.Get(dest_url)
+	if err != nil {
+		log.Fatal("fetch from net fail", err)
+	}
 
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatal("read fail", err)
-    }
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("read fail", err)
+	}
 
-    index := bytes.IndexByte(body, '\n')
-    body = body[index:]
+	index := bytes.IndexByte(body, '\n')
+	body = body[index:]
 
-    var infos interface{}
-    err = json.Unmarshal(body, &infos)
-    if err != nil {
-        log.Fatal("unmarshalling fail", err)
-    }
-    return infos
+	var infos interface{}
+	err = json.Unmarshal(body, &infos)
+	if err != nil {
+		log.Fatal("unmarshalling fail", err)
+	}
+	return infos
 }
 
 func main() {
-    printRawMarshalled(fetchChanges("merged", FirstSortKey), 0)
+	printRawMarshalled(fetchChanges("merged", FirstSortKey), 0)
 }
